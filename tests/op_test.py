3e83b76b70bdf21e5ade36e5c2616a15d46719df
AntoineDao <antoinedao1@gmail.com>
2020-04-18 21:38:03 +0100

    test(op_test): fix dependency

--- tests/op_test.py
+++ tests/op_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from ladybug.datatype.temperature import DryBulbTemperature
 from ladybug.datatype.energyintensity import DirectNormalRadiation
 from ladybug.epw import EPW
-from ladybug_pandas.factories.datatype import LadybugTypeFactory, LadybugDtypeFactory
+from ladybug_pandas.factories.datatype import LadybugDtypeFactory
 from ladybug_pandas.factories.arraytype import LadybugArrayFactory
 
 @pytest.fixture()
@@ -14,9 +14,8 @@ def temp_array(dtype):
 
 @pytest.fixture()
 def dnr_array():
-    dnr_type = LadybugTypeFactory(DirectNormalRadiation)
 
-    dnr_dtype = LadybugDtypeFactory(dnr_type)
+    dnr_dtype = LadybugDtypeFactory(DirectNormalRadiation)
 
     return LadybugArrayFactory(dnr_dtype())
 
