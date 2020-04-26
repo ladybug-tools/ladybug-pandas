ebfbb0e31c1652b2f1a40e925fb18237c57028ef
AntoineDao <antoinedao1@gmail.com>
2020-04-26 13:31:22 +0100

    docs(series): add series generator docs

--- ladybug_pandas/series.py
+++ ladybug_pandas/series.py
@@ -2,6 +2,15 @@ import pandas as pd
 from .extension_types.arraytype import LadybugArrayType
 
 def series_from_collection(data_collection) -> pd.Series:
+    """Generate a pandas Series from a Ladybug Data Collection
+
+    Arguments:
+        data_collection {ladybug._datacollectionbase.BaseCollection} -- An instance of a ladybug data collection
+
+    Returns:
+        pd.Series -- A pandas Series of type Ladybug Array
+    """
+
     array = LadybugArrayType._from_data_collection(collection)
 
     return pd.Series(
